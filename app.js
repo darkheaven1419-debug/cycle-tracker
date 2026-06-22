ec01963cb1ab98574c32bed80e838cab90c528eb
darkheaven1419-debug <darkheaven1419@github.com>
2026-06-23 01:57:44 +0800

    fix: bump SW cache to v11 to force refresh of redesigned stats module

--- app.js
+++ app.js
@@ -1556,7 +1556,7 @@ async function bootApp() {
   if (loader) { loader.style.display = 'none'; if (loader.parentNode) loader.parentNode.removeChild(loader); }
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js?v=7').catch(function(){});
+    navigator.serviceWorker.register('sw.js?v=8').catch(function(){});
   }
   loadPerProfileSettings();
 
