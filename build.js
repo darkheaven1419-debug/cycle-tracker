2ffb6cdc5b2f63435de320fd1e8642d80830ac30
darkheaven1419-debug <darkheaven1419@github.com>
2026-06-30 14:15:55 +0800

    fix: preserve lang/theme/activeProfile in terser to prevent cross-file mangling

--- build.js
+++ build.js
@@ -216,7 +216,7 @@ if (terserAvailable) {
         } else {
           Terser.minify(content, {
             compress: { passes: 2, drop_console: false },
-            mangle: { reserved: ['Lunar', 'AuthModule', 'ChartRenderer', 'CultureCardsModule', 'syncDiaryData'] },
+            mangle: { reserved: ['Lunar', 'AuthModule', 'ChartRenderer', 'CultureCardsModule', 'syncDiaryData', 'lang', 'theme', 'activeProfile', 'state'] },
             output: { comments: false },
           })
             .then(function (result) {
